a03f27a4c80432965e09f88ba37f58d87c7a6a75
Vladislav Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2016-07-01 20:22:44 +0300

    Fix compilation

--- xstream/src/java/com/thoughtworks/xstream/XStream.java
+++ xstream/src/java/com/thoughtworks/xstream/XStream.java
@@ -352,7 +352,6 @@ public class XStream {
         this(null, (Mapper)null, new XppDriver());
     }
 
-    @Deprecated
     public XStream(boolean is141Compatible) {
         this(null, (Mapper) null, new XppDriver(), is141Compatible);
     }
@@ -385,7 +384,6 @@ public class XStream {
         this(null, (Mapper)null, hierarchicalStreamDriver);
     }
 
-    @Deprecated
     public XStream(HierarchicalStreamDriver hierarchicalStreamDriver, boolean is141Compatible) {
         this(null, (Mapper) null, hierarchicalStreamDriver, is141Compatible);
     }
@@ -428,7 +426,6 @@ public class XStream {
      * {@link #XStream(ReflectionProvider, HierarchicalStreamDriver, ClassLoader, Mapper)}
      * instead
      */
-    @Deprecated
     public XStream(
             ReflectionProvider reflectionProvider, Mapper mapper, HierarchicalStreamDriver driver, boolean is141Compatible) {
         this(reflectionProvider, driver, new CompositeClassLoader(), mapper, is141Compatible);
@@ -493,7 +490,6 @@ public class XStream {
      * @deprecated As of 1.4.5 use
      *             {@link #XStream(ReflectionProvider, HierarchicalStreamDriver, ClassLoaderReference, Mapper)}
      */
-    @Deprecated
     public XStream(
             ReflectionProvider reflectionProvider, HierarchicalStreamDriver driver,
             ClassLoader classLoader, Mapper mapper, boolean is141Compatible) {
@@ -538,7 +534,6 @@ public class XStream {
         });
     }
 
-    @Deprecated
     private XStream(
             ReflectionProvider reflectionProvider, HierarchicalStreamDriver driver, ClassLoaderReference classLoader,
             Mapper mapper, final DefaultConverterLookup defaultConverterLookup, boolean is141Compatible) {
@@ -609,7 +604,6 @@ public class XStream {
         this(reflectionProvider, driver, classLoaderReference, mapper, converterLookup, converterRegistry, true);
     }
 
-    @Deprecated
     public XStream(
             ReflectionProvider reflectionProvider, HierarchicalStreamDriver driver,
             ClassLoaderReference classLoaderReference, Mapper mapper, ConverterLookup converterLookup,
